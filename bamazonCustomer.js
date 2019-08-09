b0d3487e64d675f5d81c3864cae5bd10c805bd08
Philip Vieira <philipvieira00@gmail.com>
2019-08-09 19:31:44 -0400

    Finished the project.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,11 +11,41 @@ var connection = mysql.createConnection({
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     connection.query("SELECT * FROM productsList", function(err, results) {
-        if (err) throw err;
-        var listingArray = [];
-        for (let i = 0; i<results.length; i++) {
-            listingArray.push(results[i]);
-        }
+        listingArray = results;
         console.log(listingArray);
+            inquirer.prompt([
+                {
+                    name: "betaId",
+                    type: "input",
+                    message: "What is the ID of the item you would like to purchase?"
+                },
+                {
+                    name: "purchase",
+                    type: "input",
+                    prompt: "How many would you like to purchase?"
+                }
+            ]).then(function(answer) {
+                let alphaId = answer.betaId;
+                let alphaPrice = 0;
+                let alphaAmount = 0;
+                for (let i = 0; i<listingArray.length; i++) {
+                    if (listingArray[i].item_id == answer.betaId) {
+                        alphaAmount = listingArray[i].stock -answer.purchase;
+                        alphaPrice = listingArray[i].price* answer.purchase;
+                        alphaName = listingArray[i].product_name;
+                        if (alphaAmount>=0) {
+                        connection.query("UPDATE productsList SET ? WHERE ?", [{stock: alphaAmount},{item_id: alphaId}], function(err, results) {
+                            console.log("Thank you for purchasing " + answer.purchase + " " + alphaName + ". This has cost you " + alphaPrice + "$");
+                            connection.end();
+                        })
+                        i = listingArray.length;
+                    }
+                    else {
+                        console.log("Unfortunately, we do not have sufficient stonks to deal with your request right now.");
+                        connection.end();
+                    }
+                    }
+                }
+            })
     })
-  });
+})
